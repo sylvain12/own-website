b6c0e30765c3b716507b213ba13a799f534f92e1
Sylvain Kadjo <sylvainka12@gmail.com>
2022-07-12 14:01:48 +0000

    Change index page text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hello i'm Sylvain Kadjo</h1>
+<p>Solftware engineer</p>
