c550601527d74cc181cd217825e817e0cd4a990b
Sylvain Kadjo <sylvainka12@gmail.com>
2022-07-12 14:09:19 +0000

    Add content to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Hello i'm Sylvain Kadjo</h1>
+<script>
+  let firstName = "Sylvain";
+  let lastName = "Kajo";
+  $: fullname = firstName + " " + lastName;
+
+</script>
+
+<svelte:head>
+  <title>Hi 👋 I'm {firstName}!</title>
+</svelte:head>
+
+<h1>Hi 👋 I'm {fullname}</h1>
 <p>Solftware engineer</p>
