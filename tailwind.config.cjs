d1b48b24e0d5a48fb86dc043c2700d6be786cdd5
Sylvain Kadjo <sylvainka12@gmail.com>
2022-07-24 16:08:13 +0000

    Index page
    - Add content on home page
    - Add assets: Logo, favicon
    - Add custom style

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      sans: ['var(--ff-sans)'],
+      serif: ['var(--ff-serif)']
+    },
+    colors: {
+      main: {
+        DEFAULT: '#1E81B0',
+        light: '#C7C7C7',
+      },
+      second: '#da532c'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
