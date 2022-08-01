92f4d237509ddb955178378ccc76e9acd7dbb818
Sylvain Kadjo <sylvainka12@gmail.com>
2022-08-01 12:12:14 +0000

    Change title arrow left

--- src/components/ui/Title.svelte
+++ src/components/ui/Title.svelte
@@ -19,7 +19,7 @@
   </p>
 {:else if icon?.position === Direction.Right}
   <p class="title title-icon-right">
-     <svg class="text-{icon.color == 1 ? 'main': 'second'}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M2 13.5a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 0-1H3.707L13.854 2.854a.5.5 0 0 0-.708-.708L3 12.293V7.5a.5.5 0 0 0-1 0v6z"/></svg>
+    <svg class="text-{icon.color == 1 ? 'main': 'second'}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 8v10h10"/></svg>
     {title}
   </p>
 {:else}
