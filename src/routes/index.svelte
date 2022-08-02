52a8f83091df2a7c15fd7dadacf70afef09170ed
Sylvain Kadjo <sylvainka12@gmail.com>
2022-08-02 15:17:13 +0000

    Rename Header section in Top

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
   import {owner} from '../stores/social.store'
-  import Header from '../components/Header/Index.svelte'
+  import Top from '../components/Top/Index.svelte'
   import About from '../components/About/Index.svelte'
   import Contact from '../components/Contact/Index.svelte'
   import Work from '../components/Work/Index.svelte'
@@ -12,7 +12,7 @@
   <title>Hi 👋 I'm {$owner.identification.firstName}!</title>
 </svelte:head>
 
-<Header />
+<Top />
 <About />
 <Work />
 <Contact />
